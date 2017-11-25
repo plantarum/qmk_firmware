75bafbc1bc1210dfb4defd650110f270cd48f28c
Tyler Smith <tyler@plantarum.ca>
2017-11-25 16:17:02 -0500

    config for one shot

--- keyboards/atreus/config.h
+++ keyboards/atreus/config.h
@@ -71,6 +71,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
 )
 
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped this number of times again. */
+#define ONESHOT_TIMEOUT 500  /* Time (in ms) before the one shot key is released */
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
