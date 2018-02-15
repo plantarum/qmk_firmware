468d53ebb1cf9a79d3816da0db7d2894618894fb
Tyler Smith <tyler@plantarum.ca>
2018-02-14 20:58:11 -0500

    added osl access to the lower layer, redid that layout

--- keyboards/atreus/keymaps/tws2/keymap.c
+++ keyboards/atreus/keymaps/tws2/keymap.c
@@ -14,10 +14,10 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = { /* Qwerty */
   {KC_Q,    KC_W,    KC_E,    KC_R,          KC_T,    KC_TRNS,  KC_Y,    KC_U,     KC_I,    KC_O,     KC_P    },
-  {MT(MOD_LSFT, KC_A),    MT(MOD_LCTL, KC_S),    MT(MOD_LALT, KC_D),    KC_F,          KC_G,    KC_TRNS,
-   KC_H,    KC_J,     MT(MOD_RALT, KC_K),    MT(MOD_RCTL, KC_L),     MT(MOD_RSFT, KC_SCLN) },
-  {KC_Z,    KC_X,    KC_C,    KC_V,          KC_B,    KC_TAB,  KC_N,    KC_M,     KC_COMM, KC_DOT,   KC_SLSH },
-  {KC_ESC,  KC_TAB,  KC_LGUI, OSL(_RS), KC_BSPC, KC_ENT,  KC_SPC,  OSL(_RS), KC_QUOT, KC_MINS, KC_EQUAL  }
+  {MT(MOD_LALT, KC_A),    KC_S,    KC_D,    KC_F,          KC_G,    KC_TRNS,
+   KC_H,    KC_J,     KC_K,    KC_L,     MT(MOD_RALT, KC_SCLN) },
+  {MT(MOD_LSFT, KC_Z),    KC_X,    KC_C,    KC_V,          KC_B,    KC_TAB,  KC_N,    KC_M,     KC_COMM, KC_DOT,   MT(MOD_RSFT, KC_SLSH) },
+  {MT(MOD_LCTL, KC_ESC),  OSL(_LW),  KC_LGUI, OSL(_RS), KC_BSPC, KC_ENT,  KC_SPC,  OSL(_RS), KC_QUOT, KC_MINS, MT(MOD_RCTL, KC_EQUAL)  }
 },
 /*
  *  !       @     #     $     %        ||     ^    &     *     (    )
@@ -38,10 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
  */
 [_LW] = { /* [> LOWER <] */
-  {KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS, KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10},
-  {KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN, KC_TRNS, KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11},
-  {KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   KC_LCTL, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12},
-  {KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS}
+  {KC_F9, KC_F10,  KC_F11,  KC_F12,  KC_END,  KC_TRNS, KC_PGUP, KC_F7,   KC_F8, KC_F9,   KC_F10},
+  {KC_F5, KC_F6,   KC_F7,   KC_F8,   KC_HOME, KC_TRNS, KC_DOWN, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT},
+   // KC_F4,   KC_F5,   KC_F6,   KC_F11
+  {KC_F1, KC_F2,   KC_F3,   KC_F4,   RESET,   KC_LCTL, KC_HOME, KC_F1,   KC_F2,   KC_F3,   KC_F12},
+  {KC_NO, OSL(_LW), KC_LGUI, KC_LSFT, KC_BSPC, KC_LALT, KC_SPC, TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS}
 }};
 
 const uint16_t PROGMEM fn_actions[] = {
