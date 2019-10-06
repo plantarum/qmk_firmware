15a8f9eab0dc58db93067b3d2db82240d9830048
Tyler Smith <tyler@plantarum.ca>
2019-10-06 13:50:55 -0400

    Clarified formatting

--- keyboards/atreus/keymaps/algernon/keymap.c
+++ keyboards/atreus/keymaps/algernon/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_A,   KC_S,    KC_D,  KC_F,  KC_G,  KC_TRNS,        KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN},
     {KC_Z,   KC_X,    KC_C,  KC_V,  KC_B,  OSM(MOD_LSFT),  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH},
     {KC_TAB, OSL(_LW),  OSM(MOD_LGUI), OSM(MOD_LALT), KC_BSPC,
-     KC_ENT,  KC_SPC,  OSM(MOD_RCTL), OSL(_RS), KC_MINS, KC_EQUAL}
+                                       KC_ENT,  KC_SPC,  OSM(MOD_RCTL), OSL(_RS), KC_MINS, KC_EQUAL}
 },
 /*
  *  !     @     #     $     %         ||         ^    &     *     (    )
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  _QW   _LW   win   alt   del shift ||  enter  sp   ctl   _RS   -    =
  */
 [_RS] = {                       /* [> RAISE <] */
-  {KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN},
-  {KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0},
-  {KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_GRV,  KC_TRNS, KC_TILD, KC_QUOT, KC_DQT,  KC_BSLS, KC_PIPE},
-  {TO(_QW), KC_TRNS,  KC_TRNS, KC_TRNS, KC_DELT, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
+  {KC_EXLM, KC_AT,    KC_HASH, KC_DLR,  KC_PERC, KC_TRNS,   KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN},
+  {KC_1,    KC_2,     KC_3,    KC_4,    KC_5,    KC_TRNS,   KC_6,     KC_7,    KC_8,    KC_9,    KC_0},
+  {KC_LBRC, KC_RBRC,  KC_LCBR, KC_RCBR, KC_GRV,  KC_TRNS,   KC_TILD,  KC_QUOT, KC_DQT,  KC_BSLS, KC_PIPE},
+  {TO(_QW), KC_TRNS,  KC_TRNS, KC_TRNS, KC_DELT, KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
 },
 /*
  * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
@@ -53,11 +53,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *       voldn  super shift bksp ctrl || alt space   L0  prtsc scroll pause
  */
 [_LW] = { /* [> LOWER <] */
-  {KC_F9, KC_F10,  KC_F11,  KC_F12,  KC_END,  KC_TRNS, KC_PGUP, KC_F7,   KC_F8, KC_F9,   KC_F10},
-  {KC_F5, KC_F6,   KC_F7,   KC_F8,   KC_HOME, KC_TRNS, KC_ESC, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT},
-   // KC_F4,   KC_F5,   KC_F6,   KC_F11
-  {KC_F1, KC_F2,   KC_F3,   KC_F4,   RESET,   KC_LCTL, KC_HOME, KC_F1,   KC_F2,   KC_F3,   KC_ESC},
-  {TO(_QW), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_QW)}
+  {KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_END,    KC_TRNS, KC_PGUP, KC_F7,   KC_F8,   KC_F9,   KC_F10},
+  {KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_HOME,   KC_TRNS, KC_ESC,  KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT},
+  {KC_F1,   KC_F2,   KC_F3,   KC_F4,   RESET,     KC_LCTL, KC_HOME, KC_F1,   KC_F2,   KC_F3,   KC_ESC},
+  {TO(_QW), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_QW)}
 }};
 
 const uint16_t PROGMEM fn_actions[] = {
